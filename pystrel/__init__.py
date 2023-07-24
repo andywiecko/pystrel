d39d759bcc7d6704a2519d5e7d4f4cd09a5c96ee
andywiecko <andywiecko@gmail.com>
2023-07-24 15:43:15 +0000

    feat: project utility
    Add utility for state projection.

--- pystrel/__init__.py
+++ pystrel/__init__.py
@@ -4,6 +4,6 @@ from . import combinadics
 from . import terms
 from . import operators
 from . import spectrum
-from .laboratory import measure
+from .laboratory import measure, project
 
 __version__ = "0.0.0"
--- tests/test_laboratory.py
+++ tests/test_laboratory.py
@@ -1,5 +1,6 @@
 import pytest
 import numpy as np
+import numpy.testing as npt
 import scipy.sparse as nps  # type: ignore
 import pystrel as ps
 
@@ -21,3 +22,22 @@ import pystrel as ps
 )
 def test_measure(operator, state, expected):
     assert ps.measure(operator, state) == expected
+
+
+@pytest.mark.parametrize(
+    "phi, psi, expected",
+    [
+        (np.array([0.0, 1]), np.array([1.0, 0]), 0.0),
+        (np.array([1.0, 0]), np.array([1.0, 0]), 1.0),
+        (np.array([1.0, 1.0]) / np.sqrt(2), np.array([1.0, -1.0]) / np.sqrt(2), 0.0),
+        (np.array([1.0, 1.0j]) / np.sqrt(2), np.array([1.0, 1.0j]) / np.sqrt(2), 1.0),
+        (np.array([1.0, 1.0j]) / np.sqrt(2), np.array([1.0, -1.0j]) / np.sqrt(2), 0.0),
+        (
+            np.array([0.5 + 0.5j, 0.75 + 0.25j]) / np.sqrt(2),
+            np.array([0.3 - 0.7j, 0.9 - 0.1j]) / np.sqrt(2),
+            0.210625,
+        ),
+    ],
+)
+def test_project(phi, psi, expected):
+    npt.assert_almost_equal(ps.project(phi, psi), expected)
