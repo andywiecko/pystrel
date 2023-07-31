fa548616f865fec6b8c5e46fae865cb9bd174333
andywiecko <andywiecko@gmail.com>
2023-07-31 14:53:35 +0000

    feat: typing for operators

--- pystrel/operators/__init__.py
+++ pystrel/operators/__init__.py
@@ -3,10 +3,13 @@ r"""
 
 | tag          | operator       |        name                        | `kwargs`        |
 |--------------|----------------|------------------------------------|-----------------|
-| `H`          | $H$            | Hamiltonian                        |   None          |
-| `N`          | $N=\sum_i n_i$ | total particle number operator     |   None          |
-| `n`          | $n_i$          | particle number operator           | `i`: site index |
+| [`"H"`][H]   | $H$            | Hamiltonian                        |   None          |
+| [`"N"`][N]   | $N=\sum_i n_i$ | total particle number operator     |   None          |
+| [`"n"`][n]   | $n_i$          | particle number operator           | `i`: site index |
 
+[H]: operators/impl.html#Operator_H
+[N]: operators/impl.html#Operator_N
+[n]: operators/impl.html#Operator_n
 """
 from .impl import *
 from . import utils
---
+++ pystrel/operators/typing.py
@@ -0,0 +1,6 @@
+"""
+Alias for operators for mypy hints.
+"""
+import typing
+
+Operator: typing.TypeAlias = typing.Literal["H", "N", "n"]
